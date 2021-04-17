3b1551148f5264075b1f3be73e1f5d9c7d1b8535
RjD402 <rdroid302@gmail.com>
2021-04-17 14:55:14 +0530

    matrixChainMultiplicationTopDown.cpp

---
+++ DP/matrixChainMultiplicationTopDown.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+//top down solution of matrix chain multiplication problem
+int t[101][101];
+int solve(int arr[], int i, int j){
+    //base condition for a single array
+    if(i>=j)
+    return 0;
+
+    if(t[i][j]!=-1)
+    return t[i][j];
+
+    //returning the minimum cost by placing brackets between i to j
+    //eg (A)(BCD), (AB)(CD), (ABC)(D)
+    int mn = INT_MAX;
+    for(int k=i; k<j; k++){
+        //cost of multiplying the result matrices of the two solve function
+        int cost = arr[i-1]*arr[k]*arr[j];
+        int temp = solve(arr, i,k) + solve(arr,k+1,j)+ cost;
+        if(temp<mn) mn = temp;
+    }
+    
+    return t[i][j] = mn;
+}
+
+
+int main(){
+    int arr[] = { 1, 2, 3, 4, 3 };
+    int n = sizeof(arr)/sizeof(arr[0]);
+    memset(t, -1, sizeof(t));
+    cout<<solve(arr, 1,n-1)<<endl;
+    return 0;
+}
